c1f6d8aa848321e262ccf08dafbc04aee5b5fc9a
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-03 10:21:19 -0400

    * created Book Constructor * created getters/setters for Book

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,57 @@
 package com.pluralsight;
 
 public class Book {
+    private int bookID;
+    private String isbn;
+    private String title;
+    private boolean isCheckedOut;
+    private String isCheckedOutTo;
+
+    public Book(int bookID, String isbn, String title, boolean isCheckedOut, String isCheckedOutTo) {
+        this.bookID = bookID;
+        this.isbn = isbn;
+        this.title = title;
+        this.isCheckedOut = isCheckedOut;
+        this.isCheckedOutTo = isCheckedOutTo;
+    } // Book Constructor
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getIsCheckedOutTo() {
+        return isCheckedOutTo;
+    }
+
+    public void setIsCheckedOutTo(String isCheckedOutTo) {
+        this.isCheckedOutTo = isCheckedOutTo;
+    }
 }
